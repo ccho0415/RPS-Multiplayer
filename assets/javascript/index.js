0af429249abde847725a027b17e6b5efe19f0cc3
ccho0415 <ccho0415@gmail.com>
2017-01-04 12:40:14 -0500

    starting javascript for the game

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
-	console.log("ready!");
+console.log("hi");
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQVu06TGTWcvConzQ0WWC08AZOmhjAIsU",
+    authDomain: "rps-multiplayer-a7a5c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-a7a5c.firebaseio.com",
+    storageBucket: "rps-multiplayer-a7a5c.appspot.com",
+    messagingSenderId: "687688931226"
+  };
+ firebase.initializeApp(config);
+var database = firebase.database();
+var users = [];
+$(document).ready( function(){
+$("#user").show();
+$("#gameField").hide();
+$("#score").hide();
+});
+database.ref().on("value", function(snapshot){
+
+});
+$("#addName").on("click", function(){
+event.preventDefault();
+console.log("hi I work");
+username = $("#nameInput").val().trim();
+users.push(username);
+console.log(users);
+database.ref().push({
+	username: users
+});
+
+return false;
 });
