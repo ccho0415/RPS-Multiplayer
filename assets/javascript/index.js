f02651e10b0db9d1c345f1af1502d6dc9d1015fc
ccho0415 <ccho0415@gmail.com>
2017-01-18 15:08:01 -0500

    added trainer pics. got game component and firebase working. How to link?

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,37 +8,98 @@ console.log("hi");
     messagingSenderId: "687688931226"
   };
  firebase.initializeApp(config);
+// DOM Elements
+var emailInput = $("#emailInput");
+var passwordInput = $("#passwordInput");
+var btnLogin = $("#btnLogin");
+var btnSignUp = $("#btnSignUp");
+var btnLogout = $("#btnLogout");
+var trainers = $("#trainers");
+var charmander = $("#rockModel");
+var squirtle = $("#paperModel");
+var bulbasaur = $("scissorsModel");
+// Global Vars
 var database = firebase.database();
-var users = [];
+var provider = new firebase.auth.GoogleAuthProvider();
 var username = "";
 var wins = 0;
 var losses = 0;
 var ties = 0;
 $(document).ready( function(){
   $("#user").show();
-  $("#gameField").hide();
-  $("#score").hide();
+  $("#gameField").show();
+  $("#score").show();
 });
 
-database.ref("users/").on("value", function(snapshot){ 
-  let data = snapshot.val();
-  for (let entry in data) {
-    console.log(entry);
-    $("#dummy").text(entry);
-  }
-  // console.log(snapshot.val());
-  // $("#dummy").text(snapshot.val().username);
+// Sign Up Event
+btnSignUp.click(e=>{
+  // Check for Real Email
+  // Get signup info
+  let email = emailInput.val().trim();
+  let pass = passwordInput.val().trim();
+  let auth = firebase.auth();
+  let promise =  auth.createUserWithEmailAndPassword(email,pass);
+  promise.catch(e => console.log(e.message));
 });
-$("#addName").on("click", function(){
-  event.preventDefault();
+// Login Event
+btnLogin.click( e => {
   console.log("hi I work");
-  username = $("#nameInput").val().trim();
-  console.log(username);
-  database.ref("users/" + username).set({
-	 username: username,
-   wins: wins,
-   losses: losses,
-   ties: ties
-});
+  // Get login info
+  let email = emailInput.val().trim();
+  let pass = passwordInput.val().trim();
+  let auth = firebase.auth();
+  let promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+
   return false;
 });
+btnLogout.click(e=>{
+  firebase.auth().signOut();
+});
+function startGame(){
+  if (user1 == charmander && user2 == squirtle){
+          console.log('user 2 won');
+  }else if(user1 == bulbasaur && user2 == squirtle){
+          console.log('user 1 won');
+  }else if(user1 == squirtle && user2 == charmander){
+          console.log('user 1 won');
+  }else if(user1 == bulbasaur && user2 == charmander){
+          console.log('user 2 won');
+  }else if(user1 == charmander && user2 == bulbasaur){
+          console.log('user 1 won');
+  }else if(user1 == squirtle && user2 == bulbasaur){
+          console.log('user 2 won');
+  }else if(user1 == charmander && user2 == charmander){
+          console.log('this is a tie');
+  }else if(user1 == squirtle  && user2 == squirtle ){
+          console.log('this is a tie');
+  }else if(user1 == bulbasaur && user2 == bulbasaur)
+          console.log('this is a tie'); 
+};
+// Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+    console.log(firebaseUser);
+    btnLogout.removeClass("hide");
+    trainers.removeClass("hide");
+  } else{
+    console.log("not logged in");
+    btnLogout.addClass("hide");
+    trainers.addClass("hide");
+  }
+});
+// Obtaining UID information
+  // let user =firebase.auth().currentUser;
+  // let uid;
+  // if (user!=null){
+  //   uid = user.uid;
+  //   console.log(uid);
+  // }
+  //   console.log(username);
+//   database.ref("users/" + uid).set({
+//   username: uid,
+//    email: email,
+//    wins: wins,
+//    losses: losses,
+//    ties: ties
+// });
---
+++ assets/javascript/test.js
@@ -0,0 +1,40 @@
+var charmander = $("#rockModel");
+var squirtle = $("#paperModel");
+var bulbasaur = $("scissorsModel");
+var charmanderClick  = 0;
+var squirtleClick = 0;
+var bulbasaurClick = 0;
+var user1, user2;
+	charmander.on("click", function(event){
+		charmanderClick = 1;
+		user1 = charmanderClick;
+		console.log(user1);
+	})
+	squirtle.on("click", function(event){
+		squirtleClick = 2;
+		user2 = squirtleClick;
+		console.log(squirtleClick);
+	})
+function process(){
+  if (user1 == charmanderClick && user2 == squirtleClick){
+          console.log('user 2 won');
+  }else if(user1 == bulbasaurClick && user2 == squirtleClick){
+          console.log('user 1 won');
+  }else if(user1 == squirtleClick && user2 == charmanderClick){
+          console.log('user 1 won');
+  }else if(user1 == bulbasaurClick && user2 == charmanderClick){
+          console.log('user 2 won');
+  }else if(user1 == charmanderClick && user2 == bulbasaurClick){
+          console.log('user 1 won');
+  }else if(user1 == squirtleClick && user2 == bulbasaurClick){
+          console.log('user 2 won');
+  }else if(user1 == charmanderClick && user2 == charmanderClick){
+          console.log('this is a tie');
+  }else if(user1 == squirtleClick  && user2 == squirtleClick ){
+          console.log('this is a tie');
+  }else if(user1 == bulbasaurClick && user2 == bulbasaurClick)
+          console.log('this is a tie'); 	
+}
+$("#test").on("click", function(){
+	process();
+});
